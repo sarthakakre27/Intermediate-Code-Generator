43eb9a669567d9acf9787a720f0b54aed600268b
sarthakakre27 <sarthakakre27@gmail.com>
2022-04-14 01:09:45 +0530

    first commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+all :
+	flex icg.l
+	yacc -d icg.y -ll
+	gcc lex.yy.c y.tab.c -o icg -ll
+	rm lex.yy.c y.tab.c y.tab.h icg.tab.c icg.output
---
+++ truth.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int a,b,c;
+    for(a = 0,b=2; a < 10; a=a+2,b=b+1)
+    {
+        for(b = 0; b < a; b++)
+        {
+            c = 0;
+        }
+    }
+
+}
